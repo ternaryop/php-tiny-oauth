6390f51534568d20afe65550152f81e29832bb12
Davide Ficano <davide.ficano@gmail.com>
2023-10-22 08:44:02 +0200

    fixed nullable type

--- src/OAuthConsumer.php
+++ src/OAuthConsumer.php
@@ -5,9 +5,9 @@ namespace Ternaryop\TinyOAuth;
 class OAuthConsumer {
   public string $key;
   public string $secret;
-  public string $callback_url;
+  public ?string $callback_url;
 
-  function __construct(string $key, string $secret, string $callback_url = NULL) {
+  function __construct(string $key, string $secret, ?string $callback_url = NULL) {
     $this->key = $key;
     $this->secret = $secret;
     $this->callback_url = $callback_url;
